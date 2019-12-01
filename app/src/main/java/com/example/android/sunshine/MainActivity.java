eb6b6ad6a2c4a6f4daecfce0505e658e8323e2da
Emmaka9 <emmaka993@gmail.com>
2019-12-01 18:19:24 +0800

    Initial commit

---
+++ app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.sunshine;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.example.android.sunshine.data.SunshinePreferences;
+import com.example.android.sunshine.utilities.NetworkUtils;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mWeatherTextView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forecast);
+
+        /*
+         * Using findViewById, we get a reference to our TextView from xml. This allows us to
+         * do things like set the text of the TextView.
+         */
+        mWeatherTextView = (TextView) findViewById(R.id.tv_weather_data);
+
+
+        // TODO (4) Delete the dummy weather data. You will be getting REAL data from the Internet in this lesson.
+        /*
+         * This String array contains dummy weather data. Later in the course, we're going to get
+         * real weather data. For now, we want to get something on the screen as quickly as
+         * possible, so we'll display this dummy data.
+         */
+
+        // TODO (3) Delete the for loop that populates the TextView with dummy data
+        /*
+         * Iterate through the array and append the Strings to the TextView. The reason why we add
+         * the "\n\n\n" after the String is to give visual separation between each String in the
+         * TextView. Later, we'll learn about a better way to display lists of data.
+         */
+
+
+        // DONE (9) Call loadWeatherData to perform the network request to get the weather
+        loadWeatherData();
+    }
+
+    // Completed Create a method that will get the user's preferred location and execute your
+    //  new AsyncTask and call it loadWeatherData
+    void loadWeatherData(){
+        String location  =  SunshinePreferences.getPreferredWeatherLocation(this);
+        new FetchWeatherTask().execute(NetworkUtils.buildUrl(location));
+    }
+
+
+    // TODO done (5) Create a class that extends AsyncTask to perform network requests
+    // TODO done (6) Override the doInBackground method to perform your network requests
+    // TODO  done (7) Override the onPostExecute method to display the results of the network request
+
+    class FetchWeatherTask extends AsyncTask<URL, Void, String>{
+
+        @Override
+        protected String doInBackground(URL... urls) {
+
+
+
+            URL searchURL = urls[0];
+            Log.i("Emmaka",urls[0].toString());
+            String jsonWeatherResults = null;
+            try{
+                jsonWeatherResults = NetworkUtils.getResponseFromHttpUrl(searchURL);
+
+                String[] simpleJsonWeatherData = Open
+            }
+            catch (IOException e){
+                e.printStackTrace();
+            }
+
+            return weatherResults;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            if (s != null && !s.equals("")){
+                mWeatherTextView.setText(s);
+            }else {
+                mWeatherTextView.setText("FAILED TO GET RESULT!!");
+            }
+        }
+    }
+}
